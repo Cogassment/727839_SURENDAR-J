351c3005bed543a30c853984758222706de8fa65
Cogassment <46102794+Cogassment@users.noreply.github.com>
2019-01-11 12:51:51 +0530

    Update Register.cs

--- RegisterLoginApplication/Models/Register.cs
+++ RegisterLoginApplication/Models/Register.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.ComponentModel;
@@ -6,18 +6,17 @@ using System.ComponentModel.DataAnnotations;
 using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Mvc;
+using CompareAttribute = System.Web.Mvc.CompareAttribute;
 
 /// <summary>
 /// Demonstration about Registration Page: Property Creation/Declaration 
 /// With Required Error Message and Regular Expression Pattern
 /// </summary>
 
-
 namespace RegisterLoginApplication.Models
 {
     public class Register
     {
-      
         [Required(ErrorMessage = "First Name is Required")]
         [RegularExpression("^[a-zA-Z ]*$", ErrorMessage = "Please Enter Only Alphabets.")]
         public string FirstName { get; set; }
@@ -25,7 +24,7 @@ namespace RegisterLoginApplication.Models
         [Required(ErrorMessage = "Last Name is Required")]
         [RegularExpression("^[a-zA-Z ]*$", ErrorMessage = "Please Enter Only Alphabets.")]
         public string LastName { get; set; }
-        
+
         [Required]
         [EmailAddress]
         //[RegularExpression(@"^[a-z0-9](\.?[a-z0-9]){5,}@g(oogle)?mail\.com$", ErrorMessage = "Please Enter Correct Email Address")]
@@ -34,8 +33,9 @@ namespace RegisterLoginApplication.Models
         [Required(ErrorMessage = "Mobile number is Required")]
         [RegularExpression("^[0-9]{10}$", ErrorMessage = "Please Enter Valid Mobile Number.")]
         public string MobileNumber { get; set; }
-        
-        [Required(ErrorMessage ="Role is Required")]
+
+        [Required(ErrorMessage = "Role is Required")]
+        [RegularExpression("^[a-zA-Z ]*$", ErrorMessage = "Please Enter Only Alphabets.")]
         public string Role { get; set; }
 
         [Required(ErrorMessage = "Password is Required")]
@@ -45,9 +45,8 @@ namespace RegisterLoginApplication.Models
 
         [Required(ErrorMessage = "Confirm Password is Required")]
         [DataType(DataType.Password)]
-        [System.ComponentModel.DataAnnotations.Compare("Password")]
+        [System.ComponentModel.DataAnnotations.Compare("Password", ErrorMessage = "Password and Confirm Password must match.")]
         public string ConfirmPassword { get; set; }
-        
     }
 }
 
